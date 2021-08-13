6bd686bc632497379c6d8013aec65118e7f9cfe7
omar-b98 <59110748+omar-b98@users.noreply.github.com>
2021-08-13 19:08:29 +0100

    Last update of script
    Include more bugs fixing
    This script can name VPG and Cgw, VPN
    Also have new option of having more VPN in the same VPG

--- Cardlepoint_E3000_Router_IPsec_AWS_VPC.py
+++ Cardlepoint_E3000_Router_IPsec_AWS_VPC.py
@@ -1,36 +1,3 @@
-# Cradlepoint info step
-# -----------------------------------
-# 1. Connect to distance Cradlepoint router using its public address
-#
-# AWS VPN creation steps
-# -----------------------------------
-# 1. Create Customer Gateway
-# 2. Create Virtual Private Gateway
-# 3. Attach VGW to VPC
-# 4. Create VPN Connection using CGW + VPG
-# 5. View Downloaded config with public IP and password
-# 6. Add LAN routes to VPN table
-# 7. Propogate VPC subnet into VPG
-#
-# Cradlepoint VPN Creation step
-# ------------------------------------
-# 1. create json data of VPN
-# 2. send VPN data of the IPsec configuration using SDK functions
-#
-# AWS Code
-# ------------------------------------
-# awscli multiple profiles managed in the 2 files config and credentials in C:\Users\[user]\.aws\
-# manually edit the files, or use '$ aws configure --profile profilename
-#
-# BOTO3 ec2 guide - https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html
-#
-#
-# Cradlepoint Code
-# ------------------------------------
-# API Guide - https://customer.cradlepoint.com/s/article/NCOS-SDK-v2-0-Application-Development-Guide
-# Built on SDK v2.0 (https://github.com/cradlepoint/sdk-samples)
-#
-
 from sdk.app_template_csclient.csclient import CSClient
 import boto3
 import json
@@ -38,23 +5,17 @@ import time
 import xmltodict
 import ipaddress
 
-# Cradlepoint router informations, already mentioned in sdk setting file
-app_name = 'app_template_csclient'
-dev_client_username = 'admin'
-dev_client_ip = '166.157.83.227:8080'
-dev_client_password = 'WA2017RA002161'
-
 Cradlepoint_router_public_ip = '166.157.83.227'
-
-# Cradlepoint router lans as an example
-Primary_LAN = "192.168.1.0/24"
-Guest_LAN = "192.168.10.0/24"
-Deep = "192.168.196.0/24"
 
 aws_public_ip = "x.x.x.x"
 
 myPassPhrase = "x"
-# my_aws_lan_subnet = ['10.100.0.0/24']  AWS VPC subnet (VPC->subnets) [REQUIRED]
+aws_lan_net = "172.31.0.0/16"
+ipsecname = "x"
+
+TAGS = [[{'Key': 'Name', 'Value': 'xx'}],
+        [{'Key': 'Name', 'Value': 'xx'}],
+        [{'Key': 'Name', 'Value': 'xx'}]]
 
 cscinstance = CSClient("e3000 router api ipsec configuration")
 
@@ -126,7 +87,7 @@ def Cradlepoint_router_vpn():
         ],
         "remote_network": [
             {
-                "ip_network": "10.0.0.0/24",
+                "ip_network": "0.0.0.0/0",
                 "port": None,
                 "exclude": False
             }
@@ -165,10 +126,7 @@ def Cradlepoint_router_vpn():
 
     }
 
-    vpn_data["name"] = input("Enter e3000 router ipsec tunnel name\n")
-
-    # vpn_data["local_network"][0]["exclude"] = true
-    # vpn_data["remote_gateway"] = aws_public_ip
+    vpn_data["name"] = ipsecname
 
     add_vpn_response = cscinstance.post(vpn_api_config_url, vpn_data)
 
@@ -183,15 +141,40 @@ def aws_vpn():
     global Cradlepoint_router_public_ip
     global myPassPhrase
     global aws_public_ip
+    global ipsecname
+    global TAGS
+    '''
+    profile_name = "waltham.5glabs"#input("Enter AWS profile name: (eg:omarb98)\n")
+    myRegion = "us-east-1"#input("Enter region: (eg:us-east-2)\n")
+    vpcTag = "Test-VPC"#input("Enter VPC tag: (eg:VPC-test2)\n")
+    myRouteTable = "Test-Private-rtb"#input("Enter route table tag: (eg:vpc-test2-route-table) \n")
+    TAGS[0][0]["Value"] = input("Enter Customer Gateway tag name : \n")
+    TAGS[1][0]["Value"] = input("Enter Virtual Private Gateway tag name : \n")
+    TAGS[2][0]["Value"] = input("Enter VPN tag name : \n")
+    
+    Cradlepoint_router_public_ip = "63.47.184.88"#str(input("Enter e3000 router public ip address (eg:166.157.83.226) \n"))
+    ipsecname = "test99"#input("Enter e3000 router ipsec tunnel name\n")
 
-    profile_name = input("Enter aws profile name: (eg:omarb98)\n")
-    vpcTag = input("Enter vpc tag: (eg:VPC-test)\n")
+    #**********************************************default*********************************************************************
+    profile_name = input("Enter AWS profile name: (eg:omarb98)\n")
     myRegion = input("Enter region: (eg:us-east-2)\n")
-    myRouteTable = input("Enter route table tag: (eg:vpc-test-route-table) \n")
+    vpcTag = input("Enter VPC tag: (eg:VPC-test2)\n")
+    myRouteTable = input("Enter route table tag: (eg:vpc-test2-route-table) \n")
+    TAGS[0][0]["Value"] = input("Enter Customer Gateway tag name : \n")
+    TAGS[2][0]["Value"] = input("Enter VPN tag name : \n")
+    
+    Cradlepoint_router_public_ip = str(input("Enter e3000 router public ip address (eg:166.157.83.226) \n"))
+    ipsecname = input("Enter e3000 router ipsec tunnel name\n")
+    '''
+    profile_name = "omarb98"  # input("Enter AWS profile name: (eg:omarb98)\n")
+    myRegion = "us-west-2"  # input("Enter region: (eg:us-east-2)\n")
+    vpcTag = "VPC-test2"  # input("Enter VPC tag: (eg:VPC-test2)\n")
+    myRouteTable = "vpc-test2-route-table"  # input("Enter route table tag: (eg:vpc-test2-route-table) \n")
+    TAGS[0][0]["Value"] = "mycgw"  # input("Enter Customer Gateway tag name : \n")
+    TAGS[2][0]["Value"] = "myvpn"  # input("Enter VPN tag name : \n")
 
-    vpcTag = "VPC-test2"
-    myRegion = "us-east-2"
-    myRouteTable = "vpc-test2-route-table"
+    Cradlepoint_router_public_ip = "63.47.184.88"  # str(input("Enter e3000 router public ip address (eg:166.157.83.226) \n"))
+    ipsecname = "test99"  # input("Enter e3000 router ipsec tunnel name\n")
 
     boto3.setup_default_session(profile_name=profile_name)
     ec2 = boto3.resource('ec2', region_name=myRegion)
@@ -202,9 +185,17 @@ def aws_vpn():
 
     for vpc in vpcs:
         myVpc = vpc.id
-        # response = client.describe_vpcs(VpcIds=[vpc.id,])
-        # print(json.dumps(response, sort_keys=True, indent=4))
+
     print("\nVPC ID:", myVpc)
+
+    myVGW = None
+    for vpg in client.describe_vpn_gateways()["VpnGateways"]:
+        for vpcAtt in vpg["VpcAttachments"]:
+            if vpcAtt["VpcId"] == myVpc and vpcAtt["State"] == "attached":
+                myVGW = vpg["VpnGatewayId"]
+    if not myVGW:
+        TAGS[1][0]["Value"] = input(
+            "VPC " + myVpc + " is not attached to a Virtual Private Gateway \n Enter Virtual Private Gateway tag name : \n")
 
     response = client.describe_route_tables()
 
@@ -216,14 +207,15 @@ def aws_vpn():
     print("Route Table ID: ", myRouteID)
 
     #
-    # 1. create a customer gateway (requires Meraki MX Public IP)
+    # 1. create a customer gateway CGW
     #
 
     response = client.create_customer_gateway(
+
         BgpAsn=65000,
         PublicIp=Cradlepoint_router_public_ip,
         Type='ipsec.1',
-        DeviceName='CradlepointE3000Router',
+        DeviceName='CradlepointE3000Router_VPN_' + TAGS[2][0]["Value"],
         DryRun=False
     )
 
@@ -231,50 +223,53 @@ def aws_vpn():
     print(json.dumps(response["CustomerGateway"], sort_keys=True, indent=4))
     print("Customer GatewayID:", response["CustomerGateway"]["CustomerGatewayId"])
     myCGW = response["CustomerGateway"]["CustomerGatewayId"]
-
+    client.create_tags(Tags=TAGS[0], Resources=[myCGW])
     #
-    # 2. Create Virtual Private Gateway
+    # 2. Create Virtual Private Gateway VPG
     #
-    response = client.create_vpn_gateway(
-        Type='ipsec.1',
-    )
-
-    print("\nVirtual Private Gateway result:")
-    print(json.dumps(response["VpnGateway"], sort_keys=True, indent=4))
-    print("VGW ID:", response["VpnGateway"]["VpnGatewayId"])
-    myVGW = response["VpnGateway"]["VpnGatewayId"]
-
-    #
-    # 3. Attach VGW to VPC
-    #
-    response = client.attach_vpn_gateway(
-        VpcId=myVpc,
-        VpnGatewayId=myVGW
-    )
-
-    print("\nAttachement result:")
-    print(json.dumps(response["VpcAttachment"], sort_keys=True, indent=4))
-    print("Status:", response["VpcAttachment"]["State"])
-    status = response["VpcAttachment"]["State"]
-    print("Waiting for [attached] status (rechecking every 5s)")
-    while status != "attached":
-        time.sleep(5)
-        response = client.describe_vpn_gateways(
-            VpnGatewayIds=[
-                myVGW,
-            ],
+    if not myVGW:
+        response = client.create_vpn_gateway(
+            Type='ipsec.1',
         )
 
-        for line in response["VpnGateways"]:
-            for line2 in line["VpcAttachments"]:
-                status = line2["State"]
-                print("Status:", status)
+        print("\nVirtual Private Gateway result:")
+        print(json.dumps(response["VpnGateway"], sort_keys=True, indent=4))
+        print("VGW ID:", response["VpnGateway"]["VpnGatewayId"])
+        myVGW = response["VpnGateway"]["VpnGatewayId"]
+
+        client.create_tags(Tags=TAGS[1], Resources=[myVGW])
+        #
+        # 3. Attach VGW to VPC
+        #
+        response = client.attach_vpn_gateway(
+            VpcId=myVpc,
+            VpnGatewayId=myVGW
+        )
+
+        print("\nAttachement result:")
+        print(json.dumps(response["VpcAttachment"], sort_keys=True, indent=4))
+        print("Status:", response["VpcAttachment"]["State"])
+        status = response["VpcAttachment"]["State"]
+        print("Waiting for [attached] status (rechecking every 5s)")
+        while status != "attached":
+            time.sleep(5)
+            response = client.describe_vpn_gateways(
+                VpnGatewayIds=[
+                    myVGW,
+                ],
+            )
+
+            for line in response["VpnGateways"]:
+                for line2 in line["VpcAttachments"]:
+                    status = line2["State"]
+                    print("Status:", status)
 
     #
     # 4. Create VPN Connection using CGW + VPG
     # 5. Display VPN config data (public IP/passphrase)
     #
     response = client.create_vpn_connection(
+
         CustomerGatewayId=myCGW,
         Type='ipsec.1',
         VpnGatewayId=myVGW,
@@ -284,6 +279,9 @@ def aws_vpn():
     )
 
     myVPN = response["VpnConnection"]["VpnConnectionId"]
+
+    client.create_tags(Tags=TAGS[2], Resources=[myVPN])
+
     print("VPN ID:", myVPN)
     print("\nVPN Configuration Data (showing VPN0)\n----------------------------------")
 
@@ -332,6 +330,7 @@ def aws_vpn():
 
 if __name__ == "__main__":
     aws_vpn()
+    input("block")
     Cradlepoint_router_vpn()
     print("\n VPN Tunnel is Configured!")
     input("Press enter to close")
